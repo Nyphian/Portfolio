0cba0657a9ad520e210cff65f4ca9a39e83f4acf
Nyphian Kerubo <kerubonyphian@gmail.com>
2023-04-15 16:56:21 +0000

    react rendering

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
 import './bootstrap';
+
+import { createInertiaApp } from '@inertiajs/react'
+import { createRoot } from 'react-dom/client'
+import { InertiaProgress } from '@inertiajs/progress';
+
+InertiaProgress.init(
+createInertiaApp({
+  resolve: name => {
+    const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true })
+    return pages[`./Pages/${name}.jsx`]
+  },
+  setup({ el, App, props }) {
+    createRoot(el).render(<App {...props} />)
+  },
+}));
